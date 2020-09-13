404fd9eb4fa54988c7b4cfc0c3105d6f4c301de5
machinly <machinly@outlook.com>
2020-09-13 13:01:50 +0800

    Use stdout instead of file output

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"os"
 
 	"github.com/kataras/iris"
 )
@@ -11,18 +10,12 @@ import (
 func main() {
 	app := iris.New()
 
-	f, err := os.Create("/tmp/catch")
-	if err != nil {
-		panic(err)
-	}
 	app.Handle("POST", "/", func(ctx iris.Context) {
 		bs, err := ioutil.ReadAll(ctx.Request().Body)
 		if err != nil {
 			fmt.Println(err)
 		}
-		f.Write(bs)
-		f.Write([]byte("\n"))
-		f.Sync()
+		fmt.Println(string(bs))
 		ctx.WriteString("got it")
 	})
 
